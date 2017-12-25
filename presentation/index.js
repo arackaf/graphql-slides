8f5a0635c8209fccfda772a9e0506c35afcac21d
Adam Rackis <arackis@centralreach.com>
2017-12-25 17:58:00 -0600

    Slides work

--- presentation/index.js
+++ presentation/index.js
@@ -40,11 +40,11 @@ export default class Presentation extends React.Component {
     return (
       <Deck transitionDuration={500} theme={theme}>
         <Slide transition={["fade"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            GraphQL on the server
+          <Heading textColor="secondary">
+            GraphQL:<br />what how and why
           </Heading>
-          <Text margin="10px 0 0" textColor="secondary" size={1} fit bold>
-            With MongoDB (or anything, really)
+          <Text margin="20px 0 0" textColor="secondary" size={1} bold>
+            Adam Rackis
           </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary">
@@ -57,7 +57,7 @@ export default class Presentation extends React.Component {
         </Slide>
         <Slide transition={["fade"]} bgColor="primary">
           <Heading textColor="secondary">They're primitives</Heading>
-          <Text textColor="secondary">(but that's ok!)</Text>
+          <Text textColor="secondary">(and that's a good thing!)</Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary">
           <Heading textColor="secondary">Why GraphQL?</Heading>
@@ -67,8 +67,11 @@ export default class Presentation extends React.Component {
             <ListItem>Query language tying it together</ListItem>
           </List>
         </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading textColor="secondary">A quick tour</Heading>
+        </Slide>
         <Slide transition={["fade"]} bgColor="white">
-          <Layout style={{ marginLeft: -100 }}>
+          <Layout>
             <Appear order={1}>
               <Fill>
                 <div style={{ width: 300 }}>
@@ -99,14 +102,81 @@ type Query {
             &nbsp;
             <Appear order={2}>
               <Fill>
-                <div style={{ width: 600 }}>
+                <div style={{ width: 250 }}>
                   <Text style={{ fontSize: "36px" }} textColor="secondary">
-                    Handle the query on the server
+                    And the query
                   </Text>
                   <CodePane
-                    style={{ fontSize: "24px", width: 600 }}
+                    style={{ fontSize: "24px", width: 200 }}
                     lang="javascript"
                     source={`
+{
+  allBooks {
+    isbn
+    title
+  }
+}
+                    `}
+                  />
+                </div>
+              </Fill>
+            </Appear>
+          </Layout>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="white">
+          <div style={{ width: 600 }} style={{ marginTop: -30 }}>
+            <Text style={{ fontSize: "36px" }} textColor="secondary">
+              Get your server up and running
+            </Text>
+            <CodePane
+              style={{ fontSize: "24px", width: 850 }}
+              lang="javascript"
+              source={`
+const db = MongoClient.connect(MONGO_CONNECTION);
+const root = { db };
+const executableSchema = makeExecutableSchema({ 
+  typeDefs: schema, 
+  resolvers 
+});
+
+app.use(
+  "/graphql",
+  expressGraphql({
+    schema: executableSchema,
+    graphiql: true,
+    rootValue: root
+  })
+);
+          `}
+            />
+          </div>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="primary">
+          <Text style={{ fontSize: "36px" }} textColor="secondary">
+            Execute the query
+          </Text>
+          <Image width={839} height={294} src="img/graphiQL_basicQuery.png" />
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading style={{ fontSize: "86px" }} textColor="secondary">
+            How do we get that query working?
+          </Heading>
+          <Appear order={1}>
+            <Text margin="20px 0 0" textColor="secondary" size={1} bold>
+              You do it yourself
+            </Text>
+          </Appear>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="white">
+          <div style={{ width: 600 }}>
+            <CodePane
+              style={{ fontSize: "24px", width: 600 }}
+              lang="javascript"
+              source={`
 Query: {
   async allBooks(root, args, req, ast) {
     let db = await root.db;
@@ -117,41 +187,59 @@ Query: {
   }
 }
           `}
-                  />
-                </div>
-              </Fill>
-            </Appear>
-            &nbsp;
-            <Appear order={3}>
-              <Fill>
-                <div style={{ width: 250 }}>
-                  <Text style={{ fontSize: "36px" }} textColor="secondary">
-                    Run the query!
-                  </Text>
-                  <CodePane
-                    style={{ fontSize: "24px", width: 200 }}
-                    lang="javascript"
-                    source={`
-                    {
-                      allBooks {
-                        isbn
-                        title
-                      }
-                    }
-                    `}
-                  />
-                </div>
-              </Fill>
-            </Appear>
-          </Layout>
+            />
+          </div>
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary">
-          <Text style={{ fontSize: "36px" }} textColor="secondary">
-            Execute the query
-          </Text>
-          <Image width={839} height={294} src="img/graphiQL_basicQuery.png" />
+          <Image src="img/kidEww.jpg" />
         </Slide>
+
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading style={{ fontSize: "72px" }} textColor="secondary">
+            Lets make the query more realistic
+          </Heading>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="white">
+          <Layout>
+            <Fill>
+              <div style={{ width: 300 }}>
+                <Text style={{ fontSize: "36px" }} textColor="secondary">
+                  Declare the schema
+                </Text>
+                <CodePane
+                  style={{ fontSize: "24px" }}
+                  lang="javascript"
+                  source={`
+type Query {
+  allBooks(
+    title: String
+  ): [Book]
+}`}
+                />
+              </div>
+            </Fill>
+            <Fill>
+              <div style={{ width: 300 }}>
+                <Text style={{ fontSize: "36px" }} textColor="secondary">
+                  Declare the schema
+                </Text>
+                <CodePane
+                  style={{ fontSize: "24px" }}
+                  lang="javascript"
+                  source={`
+type Query {
+  allBooks(
+    title: String
+  ): [Book]
+}`}
+                />
+              </div>
+            </Fill>
+          </Layout>
+        </Slide>
+
         <Slide transition={["fade"]} bgColor="white">
           <Layout>
             <Fill>
@@ -181,6 +269,7 @@ for (let i = 0; i < j; i++){
             </Fill>
           </Layout>
         </Slide>
+
         <Slide transition={["fade"]} bgColor="primary">
           <Appear order={1}>
             <Heading size={6} textColor="primary" caps>
@@ -241,6 +330,9 @@ for (let i = 0; i < j; i++){
           <BlockQuote>
             <Quote>Example Quote</Quote>
             <Cite>Author</Cite>
+            <Appear order={2}>
+              <Fill />
+            </Appear>
           </BlockQuote>
         </Slide>
       </Deck>
